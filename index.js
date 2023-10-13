510b6c5d1300aa174c3ce5d7f38b2fa20e9392e3
kkauma <kylekauma@gmail.com>
2023-10-12 22:06:26 -0400

    fetch data from the movie api and test the access key is working

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "a4f7df47",
+      s: "avengers",
+    },
+  });
+  console.log(response.data);
+};
+
+fetchData();

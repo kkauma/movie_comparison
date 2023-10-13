d3fbf701b514758c9225af2187cab09e82871fef
kkauma <kylekauma@gmail.com>
2023-10-12 22:10:47 -0400

    fetch data for a single movie

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetchData = async () => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "a4f7df47",
-      s: "avengers",
+      i: "tt0848228",
     },
   });
   console.log(response.data);

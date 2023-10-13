01bfbdbf709ec070ded80e5dacae405a77013e7a
kkauma <kylekauma@gmail.com>
2023-10-12 22:17:40 -0400

    create input box and search for a movie using the api

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "a4f7df47",
-      i: "tt0848228",
+      s: searchTerm,
     },
   });
   console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector("input");
+input.addEventListener("input", (event) => {
+  fetchData(event.target.value);
+});

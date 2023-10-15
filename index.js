e3b92ba1afd4095603b16c715dd579f93d497b76
kkauma <kylekauma@gmail.com>
2023-10-15 15:28:35 -0400

    fix input element to use async/await syntax

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
 
-const onInput = (event) => {
-  fetchData(event.target.value);
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+  console.log(movies);
 };
 
 input.addEventListener("input", debounce(onInput, 500));

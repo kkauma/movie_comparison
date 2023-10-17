0d18c841077ad65a427791f19fc0aebd340410c7
kkauma <kylekauma@gmail.com>
2023-10-17 09:40:18 -0400

    remove movie reference to make automplete code reusable

--- autocomplete.js
+++ autocomplete.js
@@ -3,9 +3,10 @@ const createAutoComplete = ({
   renderOption,
   onOptionSelect,
   inputValue,
+  fetchData,
 }) => {
   root.innerHTML = `
-    <label><b>Search For A Movie</b></label>
+    <label><b>Search</b></label>
     <input class="input" />
     <div class="dropdown">
         <div class="dropdown-menu">
@@ -19,25 +20,25 @@ const createAutoComplete = ({
   const resultsWrapper = root.querySelector(".results");
 
   const onInput = async (event) => {
-    const movies = await fetchData(event.target.value);
+    const items = await fetchData(event.target.value);
 
-    if (!movies.length) {
+    if (!items.length) {
       dropdown.classList.remove("is-active");
       return;
     }
 
     resultsWrapper.innerHTML = "";
     dropdown.classList.add("is-active");
-    for (let movie of movies) {
+    for (let item of items) {
       const option = document.createElement("a");
 
       option.classList.add("dropdown-item");
-      option.innerHTML = renderOption(movie);
+      option.innerHTML = renderOption(item);
 
       option.addEventListener("click", () => {
         dropdown.classList.remove("is-active");
-        input.value = inputValue(movie);
-        onOptionSelect(movie);
+        input.value = inputValue(item);
+        onOptionSelect(item);
       });
 
       resultsWrapper.appendChild(option);

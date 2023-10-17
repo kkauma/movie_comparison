4371b5c1c7a85c3394620449ee0c3d5a508bb6da
kkauma <kylekauma@gmail.com>
2023-10-17 09:58:57 -0400

    show two movie summaries

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const autocompleteConfig = {
             ${movie.Title} (${movie.Year})
         `;
   },
-  onOptionSelect(movie) {
-    onMovieSelect(movie);
-  },
   inputValue(movie) {
     return movie.Title;
   },
@@ -31,13 +28,21 @@ const autocompleteConfig = {
 createAutoComplete({
   ...autocompleteConfig,
   root: document.querySelector("#left-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#left-summary"));
+  },
 });
 createAutoComplete({
   ...autocompleteConfig,
   root: document.querySelector("#right-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#right-summary"));
+  },
 });
 
-const onMovieSelect = async (movie) => {
+const onMovieSelect = async (movie, summaryElement) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "a4f7df47",
@@ -45,7 +50,7 @@ const onMovieSelect = async (movie) => {
     },
   });
 
-  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = (movieDetail) => {

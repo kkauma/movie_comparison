0e8b7581e4178b050668447461ba624db6c89886
kkauma <kylekauma@gmail.com>
2023-10-19 10:23:09 -0400

    add styles to highlight which movie has the higher value for each section

--- index.js
+++ index.js
@@ -65,7 +65,29 @@ const onMovieSelect = async (movie, summaryElement, side) => {
   }
 };
 
-const runComparison = () => {};
+const runComparison = () => {
+  const leftSideStats = document.querySelectorAll(
+    "#left-summary .notification"
+  );
+  const rightSideStats = document.querySelectorAll(
+    "#right-summary .notification"
+  );
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    const leftSideValue = leftStat.dataset.value;
+    const rightSideValue = rightStat.dataset.value;
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove("is-primary");
+      leftStat.classList.add("is-warning");
+    } else {
+      rightStat.classList.remove("is-primary");
+      rightStat.classList.add("is-warning");
+    }
+  });
+};
 
 const movieTemplate = (movieDetail) => {
   const dollars = parseInt(
